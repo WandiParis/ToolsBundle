6c4978a8148b740d9eb1f006db685edb853a2160
Jacques de Lamballerie <jacques.delamballerie@gmail.com>
2018-11-21 17:19:06 +0100

    add files/get-uploaded-file

--- Util/Files.php
+++ Util/Files.php
@@ -2,6 +2,10 @@
 
 namespace Wandi\ToolsBundle\Util;
 
+use Mimey\MimeTypes;
+use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class Files
 {
     /** Is file
@@ -27,7 +31,7 @@ class Files
     }
 
     /**
-     * Remove directory.
+     * Delete directory.
      *
      * @param string $target
      *
@@ -36,15 +40,84 @@ class Files
     public static function deleteDirectory(string $target): bool
     {
         if (self::isDirectory($target)) {
-            $files = glob($target.'*', GLOB_MARK);
+            $files = glob($target . '*', GLOB_MARK);
             foreach ($files as $file) {
                 self::deleteDirectory($file);
             }
-            @rmdir($target);
+            rmdir($target);
         } elseif (self::isFile($target)) {
-            @unlink($target);
+            unlink($target);
         }
 
         return true;
+    }
+
+    /**
+     * @param string $path
+     * @return UploadedFile
+     */
+    public static function getUploadedFile(string $path): ?UploadedFile
+    {
+        $mimes = new MimeTypes();
+        $pathinfo = pathinfo($path);
+
+        $basename = Strings::substrBeforeLastDelimiter($pathinfo['basename'], '.');
+        $extension = $pathinfo['extension'] ?? null;
+
+
+        if ($extension === null && ((false === copy($path, $tmpPath = sys_get_temp_dir() . '/' . uniqid())) || (false === $extension = $mimes->getExtension(mime_content_type($tmpPath))))) {
+            return null;
+        }
+
+        if($extension !== null){
+            // path width extension
+            $tmpPath = $tmpPath = sys_get_temp_dir() . '/' . uniqid() . '.' . $extension;
+            if(false === copy($path, $tmpPath)){
+                return null;
+            }
+        } else {
+            // path without extension
+            $tmpPath = $tmpPath = sys_get_temp_dir() . '/' . uniqid() ;
+            if(false === copy($path, $tmpPath) || false === $extension = $mimes->getExtension(mime_content_type($tmpPath))){
+                return null;
+            }
+
+            $oldTmpPath = $tmpPath;
+            $tmpPath .= '.' . $extension;
+
+            if(false === rename($oldTmpPath, $tmpPath)){
+                return null;
+            }
+        }
+
+        return new UploadedFile($tmpPath, $pathinfo['basename'], null, null, true);
+    }
+
+    /**
+     * @param array $paths
+     * @param array|null $extensions
+     * @return null|UploadedFile
+     */
+    public static function getRandomUploadedFile(array $paths, array $extensions = null): ?UploadedFile
+    {
+        $finder = new Finder();
+        $finder->in($paths);
+
+        if ($extensions !== null) {
+            $regex = '/(' . implode('|', $extensions) . ')/i';
+            $finder->name($regex);
+        }
+
+        $files = [];
+        foreach ($finder->files() as $file) {
+            /** @var SplFileInfo $file */
+            $files[] = $file->getPathname();
+        }
+
+        if (empty($files)) {
+            return null;
+        }
+
+        return self::getUploadedFile(Arrays::getRandom($files));
     }
 }
